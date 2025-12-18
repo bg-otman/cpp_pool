904bc27540a324538edb58a7186b7f05cecce2b3
Othmane Bouizi <bg22otman@gmail.com>
2025-12-18 14:11:26 +0100

    start cpp06/ex02

---
+++ cpp06/ex02/Base.cpp
@@ -0,0 +1,21 @@
+#include "Base.hpp"
+
+Base::Base()
+{
+}
+
+Base::Base(const Base& obj)
+{
+    (void) obj;
+}
+
+Base& Base::operator=(const Base& obj)
+{
+    (void) obj;
+    return *this;
+}
+
+Base::~Base()
+{
+}
+
---
+++ cpp06/ex02/Base.hpp
@@ -0,0 +1,18 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base
+{
+    private:
+        /* data */
+    public:
+        Base();
+        Base(const Base& obj);
+        Base& operator=(const Base& obj);
+        ~Base();
+
+        
+};
+
+
+#endif
